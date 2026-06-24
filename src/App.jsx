5e33f4a6e5383cdd8e21c5f1748d78fdb2ffc74d
raphajuve-svg <raphajuve@gmail.com>
2026-06-25 00:35:07 +0200

    Load image crop fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ChezMus from "./ChezMus";
 import "./chezmus-western-polish.css";
 import "./chezmus-mobile-ux.css";
+import "./chezmus-image-fixes.css";
 
 export default function App() {
   return <ChezMus />;
